a1898ee724ad25c46df9c49b4951a8062fc8e6d9
jenkiron <jitlok@gmail.com>
2021-03-14 19:35:05 -0600

    Movie Requests

--- Movie.js
+++ Movie.js
@@ -16,7 +16,7 @@ mongoose.set('useCreateIndex', true);
 
 
 var MovieSchema = new Schema( {
-    title: {type: String, required: true, index: {unique: true, dropDups: true}},
+    title: {type: String, required: true, index: {unique: true}},
     year: {type: Number, required: true},
     genre: {type: String, required: true, enum: ["Action", "Adventure", "Comedy", "Drama", "Fantasy", "Horror", "Mystery", "Thriller", "Western"]},
     actors: {type: Array, required: true, items: {actorName: String, characterName: String}, minItems: 3}
