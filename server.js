b2783e4f56a34b81b8a4bab0e9ff9fd346e99a2c
jenkiron <jitlok@gmail.com>
2021-04-11 15:48:29 -0600

    set avgRating in aggregate, added avgrating to movie shcema

--- server.js
+++ server.js
@@ -176,7 +176,7 @@ router.route('/reviews')
                     Movie.aggregate()
                         .match({title: req.body.title.toString()})
                         .lookup({from: 'reviews', localField: 'title', foreignField: 'movieName', as: 'Movie-Reviews'})
-                        .addFields( {avgRating: {$avg: '$reviews.rating'}})
+                        .addFields( {avgRating: {$avg: '$Movie-Reviews.rating'}})
                         .exec(function (err, result) {
                             movie.avgRating = result[0].avgRating;
                             if (err) {
