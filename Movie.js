484da7d15fe081d1793e9a218b67747d7d68d992
Ronald Jenkins <55325584+jenkiron@users.noreply.github.com>
2021-04-11 11:56:29 -0600

    Update Movie.js

--- Movie.js
+++ Movie.js
@@ -19,7 +19,8 @@ var MovieSchema = new Schema( {
     title: {type: String, required: true, index: {unique: true}},
     year: {type: Number, required: true},
     genre: {type: String, required: true, enum: ["Action", "Adventure", "Comedy", "Drama", "Fantasy", "Horror", "Mystery", "Thriller", "Western"]},
-    actors: {type: Array, required: true, items: {actorName: String, characterName: String}, minItems: 3}
+    actors: {type: Array, required: true, items: {actorName: String, characterName: String}, minItems: 3},
+    imageURL: {type: String}
 });
 
 //this is broken, movie never gets stored. it catches bad input
